ac8d6f0f8dd849f6cfd0a830e89749e1e4e39b66
ErikMarx <121640897+MarxErik@users.noreply.github.com>
2024-02-16 20:33:01 +0300

    Initial commit

---
+++ src/main/java/org/marx/spring/boot_security/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package org.marx.spring.boot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+	}
+
+}
